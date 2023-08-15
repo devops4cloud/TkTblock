8208166ed1391445816b12eb4a15bead0054f68c
Mario G <Mario G>
2023-08-15 12:13:00 -0400

    Added images and json files with a airdrop function

--- events.sol
+++ events.sol
@@ -13,7 +13,7 @@ contract myContract is ERC1155, Ownable, ERC1155Supply, Pausable {
     ERC1155("https://ipfs.io/ipfs/bafybeiawadna4crtpagegyqfpxskq4xhglkh7r6egeb257sa3kff2d7zlq/")
     {}
 
-// Create a struct to hold the event information
+    // Create a struct to hold the event information
     struct Event {
         uint _event_Id;
         string _eventName;
@@ -25,73 +25,73 @@ contract myContract is ERC1155, Ownable, ERC1155Supply, Pausable {
         uint256 _eventTicketSold;
 
     }
-// Create a mapping of events with the identifiers
+    // Create a mapping of events with the identifiers
     mapping(uint => Event) public events;
 
 
-// Create a function to add a new event
+    // Create a function to add a new event
 
     function addEvent(uint _id, string memory _eventName, string memory _eventDate, string memory _eventVenue, uint256 _eventTicketPrice, uint256 _eventTicketSupply,string memory _eventURL, uint256 _eventTicketSold) public {
             events[_id] = Event(_id, _eventName, _eventDate, _eventVenue, _eventTicketPrice, _eventTicketSupply, _eventURL, _eventTicketSold);
 
     }
 
-// Create a function to get details about the event
+    // Create a function to get details about the event
     function getEventDetails(uint _id) external view returns (uint, string  memory, string memory, string memory, uint256, uint256,string memory, uint256) {
             Event storage _event = events[_id];
             return (_event._event_Id,_event._eventName,_event._eventDate, _event._eventVenue, _event._eventTicketPrice, _event._eventTicketSupply, _event._eventURL, _event._eventTicketSold );
 
     }
 
-// Create a function to get event id
-    function get_eventId(uint _id) public view returns (uint){
+    // Create a function to get event id
+    function getEventId(uint _id) public view returns (uint){
         Event memory _eventa = events[_id];
         return _eventa._event_Id;
 
     }
 
-// Create a function to get event name
-    function get_eventName(uint _id) public view returns (string memory){
+    // Create a function to get event name
+    function getEventName(uint _id) public view returns (string memory){
         Event memory _eventa = events[_id];
         return _eventa._eventName;
 
     }
-// Create a function to update event name
-    function update_eventName(uint _id, string memory _eName) public {
+    // Create a function to update event name
+    function updateEventName(uint _id, string memory _eName) public {
         Event storage _eventa = events[_id];
         _eventa._eventName = _eName;
     }
-// Create a function to get the ticket price
-    function get_eventTicketPrice(uint _id) public view returns (uint){
+    // Create a function to get the ticket price
+    function getEventTicketPrice(uint _id) public view returns (uint){
         Event memory _eventa = events[_id];
         return _eventa._eventTicketPrice;
 
     }
-// Create a function to update the ticket price
-    function update_eventTicketPrice(uint _id, uint _eventTicketPrice) public {
+    // Create a function to update the ticket price
+    function updateEventTicketPrice(uint _id, uint _eventTicketPrice) public {
         Event storage _eventa = events[_id];
         _eventa._eventTicketPrice = _eventTicketPrice;
     }
-// Create a function to get the ticket supply
-    function get__eventTicketSupply(uint _id) public view returns (uint){
+    // Create a function to get the ticket supply
+    function getEventTicketSupply(uint _id) public view returns (uint){
         Event memory _eventa = events[_id];
         return _eventa._eventTicketSupply;
 
     }
-// Create a function to update the ticket supply
-    function update__eventTicketSupply(uint _id, uint _eventTicketSupply) public {
+    // Create a function to update the ticket supply
+    function updateEventTicketSupply(uint _id, uint _eventTicketSupply) public {
         Event storage _eventa = events[_id];
         _eventa._eventTicketSupply = _eventTicketSupply;
     }
 
-// Create a function to get the amount of tickets sold 
-    function get__eventTicketSold(uint _id) public view returns (uint){
+    // Create a function to get the amount of tickets sold 
+    function getEventTicketSold(uint _id) public view returns (uint){
         Event memory _eventa = events[_id];
         return _eventa._eventTicketSold;
 
     }
-// Create a function to update the number of tickets sold 
-    function update__eventTicketSold(uint _id, uint amount) public {
+    // Create a function to update the number of tickets sold 
+    function updateEventTicketSold(uint _id, uint amount) public {
         Event storage _eventa = events[_id];
         _eventa._eventTicketSold += amount;
     }
@@ -102,25 +102,41 @@ contract myContract is ERC1155, Ownable, ERC1155Supply, Pausable {
         public
         payable
     {
+        //Verifying the event ID is valid and the amount passed is correct.
+        require(getEventId(id) > 0, "Event ID is not valid");
 
-        //require(id <= eventTicketSupply.length, "This event does not exist"); 
-        //require(id >= 0, "This event does not exit");
-        require(get_eventId(id) > 0, "Event ID is not valid");
-        require(get__eventTicketSold(id) + amount <= get__eventTicketSupply(id), "Amount exeeds total supply or event sold out" );
-        require(msg.value == amount * get_eventTicketPrice(id), "Not enough funds sent or too much funds sent");
+        //Checking to see whether there are enough tickets available for sale
+        require(getEventTicketSold(id) + amount <= getEventTicketSupply(id), "Amount exeeds total supply or event sold out" );
+
+        // Checking the amount transfered is enough to purchase the ticket
+        require(msg.value == amount * getEventTicketPrice(id), "Not enough funds sent or too much funds sent");
+
+        //Minting the tickets
         _mint(msg.sender, id, amount, "");
 
         // Update the tickets sold variable for the event
-        update__eventTicketSold(id, amount);
+        updateEventTicketSold(id, amount);
     }
 
-        //Adding the URI function to customize the URL to match the token
+    //Adding the URI function to customize the URL to match the token for OpenSea compatibility
     function uri(uint256 _id) public view virtual override returns (string memory) {
             require (exists(_id), "URI Token does not exist");
             return string(abi.encodePacked(super.uri(_id),Strings.toString(_id),".json"));
-    
     }
-    //Adding a withdrawal contract so that 
+    //Adding a contract level URI for OpenSea compatibility
+    function contractURI() public pure returns (string memory){
+            return "https://ipfs.io/ipfs/bafybeiawadna4crtpagegyqfpxskq4xhglkh7r6egeb257sa3kff2d7zlq/collection.json";
+
+    }
+
+    // Ability for the owner to distribute tickets a.k.a airdrop
+    function airdropTickets(uint256 _id, address[] calldata recipients, uint amount) external onlyOwner {
+            for (uint i =0; i < recipients.length; i++){
+                _safeTransferFrom(msg.sender, recipients[i], _id, amount,"");
+            }
+    }
+    
+    //Adding a withdrawal contract so that only the owner can transfer
     function withdraw() public onlyOwner {
         require(address(this).balance > 0, "Balance is 0");
         payable(owner()).transfer(address(this).balance);
